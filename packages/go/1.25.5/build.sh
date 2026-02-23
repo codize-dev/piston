8552867738abdc0026f2ffb3ac9953847a9607f0
koki-develop <kou.pg.0131@gmail.com>
2026-02-24 08:38:28 +0900

    Prepare Go 1.25.5 package for predownloaded x/text module

--- packages/go/1.25.5/build.sh
+++ packages/go/1.25.5/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 curl -LO https://go.dev/dl/go1.25.5.linux-amd64.tar.gz
 tar -xzf go1.25.5.linux-amd64.tar.gz
 rm go1.25.5.linux-amd64.tar.gz
@@ -7,6 +9,11 @@ rm go1.25.5.linux-amd64.tar.gz
 CGO_ENABLED=0 ./go/bin/go build -o cacheprog cacheprog.go
 
 # Pre-build standard library cache
-export GOCACHE=$PWD/cache
-mkdir -p $GOCACHE
+export GOCACHE="$PWD/cache"
+mkdir -p "$GOCACHE"
 CGO_ENABLED=0 ./go/bin/go build std
+
+# Pre-download selected golang.org/x modules for offline builds.
+export GOPATH="$PWD/gopath"
+mkdir -p "$GOPATH"
+CGO_ENABLED=0 ./go/bin/go mod download
--- packages/go/1.25.5/go.mod
+++ packages/go/1.25.5/go.mod
@@ -1,3 +1,5 @@
 module piston
 
 go 1.25.5
+
+require golang.org/x/text v0.34.0
